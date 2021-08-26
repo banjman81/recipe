01489c4b1c1428e953793b46b27e43059c931d17
[banjman81] <[benjamin.man97@gmail.com]>
2021-08-26 18:02:05 -0500

    last commit

--- src/App.js
+++ src/App.js
@@ -9,17 +9,19 @@ function App(){
   const [recipes, setrecipes] = useState([])
   const [search, setSearch] = useState('')
   const [query, setQuery] = useState('chicken')
-
-  useEffect(() =>{
-    getRecipes();
-  }, [query]);// this [] contains the value that will update useeffect. useEffect will run every time this value is changed
-
+  
   async function getRecipes(){
     const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setrecipes(data.hits);
     console.log(data.hits);
   }
+
+  useEffect(() =>{
+    getRecipes();
+  }, [query]);// this [] contains the value that will update useeffect. useEffect will run every time this value is changed
+
+
 
   function updateSearch(e){
     setSearch(e.target.value);// ---this is require to cmake changes ot input state
